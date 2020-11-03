0fb2d601cc86b0c3c2a826f9bd8055ddea3d3cdd
Murat Tishkul <tishkul@unist.ac.kr>
2020-11-04 00:43:21 +0600

    implemented login page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
+import LoginPage from './pages/Login';
+import ResetPassword from './pages/ResetPassword'
+import NewPassword from './pages/NewPassword'
+import { LOGIN, FORGOT_PASSWORD, NEW_PASSWORD } from './helpers/routes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path={`/${LOGIN}`} component={LoginPage} />
+        <Route path={`/${FORGOT_PASSWORD}`} component={ResetPassword} /> 
+        <Route path={`/${NEW_PASSWORD}`} component={NewPassword} /> 
+        <Redirect exact from="/" to={`/${LOGIN}`} />
+      </Switch>
+    </Router>
   );
 }
 
