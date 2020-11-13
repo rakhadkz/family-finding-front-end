d688d80265d6ba0b5069ff5c4e30e497d2c5447f
Bekzat <bekzat@MacBook-Pro-Bekzat.local>
2020-11-13 12:17:18 +0600

    change authUrl

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,7 @@
 import { getToken } from "../context/auth/authProvider";
 
-//export const authURL = "https://family-finding-api.herokuapp.com/api/v1"
-export const authURL = "http://localhost:3000/api/v1";
+export const authURL = "https://family-finding-api.herokuapp.com/api/v1";
+// export const authURL = "http://localhost:3000/api/v1";
 
 export const request = async ({ endpoint, data, method }) => {
   const token = await getToken();
