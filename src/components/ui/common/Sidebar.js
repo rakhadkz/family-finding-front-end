2e18f49e3cd5b8c71241245ddf958b3b0a1eba1a
Bekzat <bekzat@MacBook-Pro-Bekzat.local>
2020-11-19 18:11:20 +0600

    sidebar by roles

--- src/components/ui/common/Sidebar.js
+++ src/components/ui/common/Sidebar.js
@@ -1,9 +1,17 @@
+import ChildIssuesIcon from "@atlaskit/icon/glyph/child-issues";
+import EmojiSymbolsIcon from "@atlaskit/icon/glyph/emoji/symbols";
+import MentionIcon from "@atlaskit/icon/glyph/mention";
 import OfficeBuilding from "@atlaskit/icon/glyph/office-building";
 import People from "@atlaskit/icon/glyph/people";
+import QueuesIcon from "@atlaskit/icon/glyph/queues";
+import Screen from "@atlaskit/icon/glyph/screen";
+import SearchIcon from "@atlaskit/icon/glyph/search";
+import SettingsIcon from "@atlaskit/icon/glyph/settings";
 import Signout from "@atlaskit/icon/glyph/sign-out";
 import React from "react";
 import { Link } from "react-router-dom";
 import { useAuth } from "../../../context/auth/authContext";
+import { GroupAccess } from "../../common";
 import { Box, Logo, SidebarMenuItem, Spacing } from "../atoms";
 import { SidebarUser } from "./SidebarUser";
 
@@ -12,11 +20,56 @@ const SIDEBAR_ITEMS = [
     to: "/",
     title: "Organizations",
     icon: () => <OfficeBuilding />,
+    exact: "super_admin",
+  },
+
+  {
+    to: "/action_items",
+    title: "Action Items",
+    icon: () => <Screen />,
+    atLeast: "manager",
+  },
+  {
+    to: "/children",
+    title: "Children",
+    icon: () => <EmojiSymbolsIcon />,
+    atLeast: "manager",
+  },
+  {
+    to: "/continuous_search",
+    title: "Continuous Searches",
+    icon: () => <SearchIcon />,
+    atLeast: "manager",
+  },
+  {
+    to: "/reports",
+    title: "Reports",
+    icon: () => <QueuesIcon />,
+    exact: "admin",
   },
   {
     to: "/users",
     title: "Users",
     icon: () => <People />,
+    exact: "super_admin",
+  },
+  {
+    to: "/search_vectors",
+    title: "Search Vectors",
+    icon: () => <ChildIssuesIcon />,
+    exact: "admin",
+  },
+  {
+    to: "/communications_templates",
+    title: "Communications Templates",
+    icon: () => <MentionIcon />,
+    exact: "admin",
+  },
+  {
+    to: "/settings",
+    title: "Settings",
+    icon: () => <SettingsIcon />,
+    atLeast: "manager",
   },
 ];
 
@@ -38,12 +91,14 @@ export const Sidebar = () => {
           </Spacing>
         </Box>
         {SIDEBAR_ITEMS.map((item) => (
-          <SidebarMenuItem key={item.to}>
-            <Link to={item.to}>
-              {item.icon()}
-              <Spacing m={{ l: "15px" }}>{item.title}</Spacing>
-            </Link>
-          </SidebarMenuItem>
+          <GroupAccess {...item}>
+            <SidebarMenuItem key={item.to}>
+              <Link to={item.to}>
+                {item.icon()}
+                <Spacing m={{ l: "15px" }}>{item.title}</Spacing>
+              </Link>
+            </SidebarMenuItem>
+          </GroupAccess>
         ))}
         <SidebarMenuItem>
           <Link
