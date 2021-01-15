b4fc26c754c9a0696493ce0be29d7c9845ce6d53
Murat Tishkul <tishkul@unist.ac.kr>
2021-01-15 23:10:18 +0600

    made fixed footer

--- src/components/ChildProfile/tabs/Comments/CommentsTab.js
+++ src/components/ChildProfile/tabs/Comments/CommentsTab.js
@@ -6,9 +6,27 @@ import { Box, Spacing } from "../../../ui/atoms";
 import { Comments } from "./Comments";
 import { CommentsForm } from "./CommentsForm";
 import { MentionsProvider } from "./mentions-context";
+import styled from "styled-components";
+
 export const CommentsTab = ({ childId, childComments, setChild }) => {
   const [comments, setComments] = useState(childComments);
   const [shouldUpdate, increaseShouldUpdate] = useState(0);
+  const [show, handleShow] = useState(false);
+  const [isExpanded, setIsExpanded] = useState(false);
+  const expandEditor = () => setIsExpanded(true);
+  const collapseEditor = () => setIsExpanded(false);
+  const [blocks, setBlocks] = useState(1);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } //else handleShow(false);
+      return () => {
+        window.removeEventListener("scroll");
+      };
+    });
+  }, []);
 
   useEffect(() => {
     fetchComments(childId).then((items) => {
@@ -20,24 +38,8 @@ export const CommentsTab = ({ childId, childComments, setChild }) => {
 
   return (
     <MentionsProvider>
-      <Spacing m={{ t: "22px" }}>
-        <Box d="flex">
-          <Avatar
-            appearance="circle"
-            src="https://pbs.twimg.com/profile_images/803832195970433027/aaoG6PJI_400x400.jpg"
-            size="large"
-          />
-          <Spacing m={{ l: "17px", t: "-22px" }}>
-            <CommentsForm
-              shouldUpdate={shouldUpdate}
-              increaseShouldUpdate={increaseShouldUpdate}
-              id={childId}
-              inReply={0}
-              onSubmit={postCommentRequest}
-            />
-          </Spacing>
-        </Box>
-        <Spacing m={{ t: "22px" }}>
+      <Spacing m={{ b: "22px" }}>
+        <Spacing m={{ b: "22px" }}>
           {comments &&
             comments
               .filter((comment) => !comment.in_reply_to)
@@ -51,7 +53,46 @@ export const CommentsTab = ({ childId, childComments, setChild }) => {
                 />
               ))}
         </Spacing>
+        <Footer d="flex" show={show} isExpanded={isExpanded} blocks={blocks}>
+          <Avatar
+            appearance="circle"
+            src="https://pbs.twimg.com/profile_images/803832195970433027/aaoG6PJI_400x400.jpg"
+            size="large"
+          />
+          <Spacing m={{ l: "17px", t: "-22px" }}>
+            <CommentsForm
+              setBlocks={setBlocks}
+              isExpanded={isExpanded}
+              collapseEditor={collapseEditor}
+              expandEditor={expandEditor}
+              shouldUpdate={shouldUpdate}
+              increaseShouldUpdate={increaseShouldUpdate}
+              id={childId}
+              inReply={0}
+              onSubmit={postCommentRequest}
+            />
+          </Spacing>
+        </Footer>
       </Spacing>
     </MentionsProvider>
   );
 };
+//margin-bottom:50px;
+
+const Footer = styled(Box)`
+  ${(props) =>
+    props.show &&
+    `
+  position: fixed;
+  height:${
+    props.show && props.isExpanded ? (200 * props.blocks).toString() : "50"
+  }px;
+  width: 100%;
+  border: solid;
+  border-radius: 5px;
+  border-color: white;
+  background-color: white;
+  bottom: 0px;
+  margin-bottom: 0px;
+  `}
+`;
