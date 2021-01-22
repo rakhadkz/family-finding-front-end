93fb298276e65b0dd18aa50f5888b5ccab5befe6
Shyngys Rakhad <dev.chinga@gmail.com>
2021-01-22 21:10:40 +0600

    fix: base url

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,7 @@
 import { getToken } from "../context/auth/authProvider";
 
 export const authURL = `${process.env.REACT_APP_API_BASE_URL}`;
+
 export const request = ({ endpoint, data, method, meta = false }) => {
   const token = getToken();
 
