6c891bbadf14a3ca4b1440629d87f0e26569c9a7
Bekzat <bekzat@MacBook-Pro-Bekzat.local>
2021-02-12 23:02:31 +0600

    numbers of possible connections

--- src/content/possibleConnection.data.js
+++ src/content/possibleConnection.data.js
@@ -1,11 +1,11 @@
-import Button from "@atlaskit/button"
-import EmailIcon from '@atlaskit/icon/glyph/email'
-import NotificationIcon from "@atlaskit/icon/glyph/notification-direct"
-import AttachmentIcon from "@atlaskit/icon/glyph/attachment"
-import React from 'react'
-import { Avatar } from "../components/ui/molecules/Avatar"
-import { Box, Spacing } from "../components/ui/atoms"
+import Button from "@atlaskit/button";
+import AttachmentIcon from "@atlaskit/icon/glyph/attachment";
 import EditorEditIcon from "@atlaskit/icon/glyph/editor/edit";
+import EmailIcon from "@atlaskit/icon/glyph/email";
+import NotificationIcon from "@atlaskit/icon/glyph/notification-direct";
+import React from "react";
+import { Box, Spacing } from "../components/ui/atoms";
+import { Avatar } from "../components/ui/molecules/Avatar";
 
 export const possibleConnectionRows = (
   data,
@@ -13,88 +13,102 @@ export const possibleConnectionRows = (
   setCurrentConnection,
   setIsConfirmModalOpen,
   setIsAddModalOpen
-) => (
+) =>
   data
-    .filter(item => item && !item.is_confirmed)
-    .map((item, index) => item && item.contact && (
-      {
-        key: index,
-        cells: [
-          {
-            key: "full_name",
-            content: (
-              <Box d="flex" align="center">
-                <Avatar name={`${item.contact.first_name} ${item.contact.last_name}`} size="medium" ratio={0.4}/>
+    .filter((item) => item && !item.is_confirmed)
+    .map(
+      (item, index) =>
+        item &&
+        item.contact && {
+          key: index,
+          cells: [
+            {
+              key: "full_name",
+              content: (
                 <Box d="flex" align="center">
-                  <Button 
-                    appearance="link" 
-                    spacing="none" 
-                    style={{marginLeft: "8px"}}
-                    onClick={() => openModal("main", item)}
-                  >
-                    {`${item.contact.first_name} ${item.contact.last_name}`}
-                  </Button>
-                  <Button spacing="none" style={{marginLeft: "5px"}} appearance="link" onClick={() => {
-                    setCurrentConnection(item)
-                    setIsAddModalOpen(true)
-                  }}>
-                    <EditorEditIcon size="medium" />
-                  </Button>
+                  <Avatar
+                    name={`${item.contact.first_name} ${item.contact.last_name}`}
+                    size="medium"
+                    ratio={0.4}
+                  />
+                  <Box d="flex" align="center">
+                    <Button
+                      appearance="link"
+                      spacing="none"
+                      style={{ marginLeft: "8px" }}
+                      onClick={() => openModal("main", item)}
+                    >
+                      {`${item.contact.first_name} ${item.contact.last_name}`}
+                    </Button>
+                    <Button
+                      spacing="none"
+                      style={{ marginLeft: "5px" }}
+                      appearance="link"
+                      onClick={() => {
+                        setCurrentConnection(item);
+                        setIsAddModalOpen(true);
+                      }}
+                    >
+                      <EditorEditIcon size="medium" />
+                    </Button>
+                  </Box>
                 </Box>
-              </Box>
-            )
-          },
-          {
-            key: "relationship",
-            content: item.relationship
-          },
-          {
-            key: "engagement",
-            content: (
-              <Box d="flex">
-                  <Button appearance="link" spacing="none" style={{ marginRight: "17px" }}>
+              ),
+            },
+            {
+              key: "relationship",
+              content: item.relationship,
+            },
+            {
+              key: "engagement",
+              content: (
+                <Box d="flex">
+                  <Button
+                    appearance="link"
+                    spacing="none"
+                    style={{ marginRight: "17px" }}
+                  >
                     <Box d="flex" align="center">
-                      <Spacing m={{r: "4px"}}>
-                        5
-                      </Spacing>
+                      <Spacing m={{ r: "4px" }}>{item.templates_size}</Spacing>
                       <EmailIcon />
                     </Box>
                   </Button>
-                  <Button appearance="link" spacing="none" style={{ marginRight: "17px" }}>
+                  <Button
+                    appearance="link"
+                    spacing="none"
+                    style={{ marginRight: "17px" }}
+                  >
                     <Box d="flex" align="center">
-                      <Spacing m={{r: "4px"}}>
-                        3
-                      </Spacing>
+                      <Spacing m={{ r: "4px" }}> {item.alerts_size}</Spacing>
                       <NotificationIcon />
                     </Box>
                   </Button>
                   <Button appearance="link" spacing="none">
                     <Box d="flex" align="center">
-                      <Spacing m={{r: "4px"}}>
-                        6
+                      <Spacing m={{ r: "4px" }}>
+                        {item.attachments_size}
                       </Spacing>
                       <AttachmentIcon />
                     </Box>
                   </Button>
-              </Box>
-            )
-          },
-          {
-            key: "actions",
-            content: (
-            <div align="center">
-              <Button
-                onClick={() => {
-                  setCurrentConnection(item)
-                  setIsConfirmModalOpen(true)
-                }}
-              >
-                Confirm
-              </Button>
-            </div>
-            )
-          }
-        ]
-      }
-    ))
-)
+                </Box>
+              ),
+            },
+            {
+              key: "actions",
+              content: (
+                <div align="center">
+                  <Button
+                    onClick={() => {
+                      setCurrentConnection(item);
+                      setIsConfirmModalOpen(true);
+                    }}
+                  >
+                    Confirm
+                  </Button>
+                </div>
+              ),
+            },
+          ],
+        }
+    );
