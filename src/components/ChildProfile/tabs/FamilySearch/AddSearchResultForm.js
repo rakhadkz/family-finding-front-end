3ff4ab5d437e7b746bc5db735057c9fda91d9572
Shyngys Rakhad <dev.chinga@gmail.com>
2021-02-24 19:15:07 +0600

    add: requests api

--- src/components/ChildProfile/tabs/FamilySearch/AddSearchResultForm.js
+++ src/components/ChildProfile/tabs/FamilySearch/AddSearchResultForm.js
@@ -6,11 +6,8 @@ import { SelectInput } from "../../../ui/molecules";
 import Select from "@atlaskit/select";
 import { WysiwygEditor } from "../../../WYSIWYG";
 import DocumentIcon from "@atlaskit/icon/glyph/document";
-import InviteTeamIcon from "@atlaskit/icon/glyph/invite-team";
 import { fetchSearchVectorsRequest } from "../../../../api/searchVectors/searchVectorsRequests";
 import { ModalDialog } from "../../../ui/common";
-//import tag from "@atlaskit/tag/dist/types/tag";
-import TagGroup from "@atlaskit/tag-group";
 import { ChildContext } from "../../../../pages/ChildProfilePage";
 
 export const AddSearchResultForm = ({
@@ -19,9 +16,20 @@ export const AddSearchResultForm = ({
 }) => {
   const { control } = useForm();
   const [options, setOptions] = useState([]);
-  const [isConnectionModalOpen, setIsConnectionModalOpen] = useState(false);
-  const [connections, setConnections] = useState(
-    useContext(ChildContext).connectionState.connections || []
+  const connections = useContext(ChildContext).connectionState.connections.map(
+    (connection) => {
+      if (connection && connection.contact) {
+        const {
+          contact: { first_name, last_name },
+          id,
+        } = connection;
+        return {
+          label: first_name + " " + last_name,
+          value: id,
+        };
+      }
+      return null;
+    }
   );
   const [assignedConnections, setAssignedConnections] = useState([]);
 
@@ -36,16 +44,6 @@ export const AddSearchResultForm = ({
     );
   };
 
-  const onAddConnection = () => {
-    assignedConnections.map((item) =>
-      setConnections((prev) => [...prev, item])
-    );
-  };
-
-  const onDeleteConnection = (id) => {
-    setConnections((prev) => prev.filter((connection) => connection.id !== id));
-  };
-
   return (
     <form>
       <SelectInput
@@ -58,67 +56,32 @@ export const AddSearchResultForm = ({
         placeholder="Select search vector"
       />
       <WysiwygEditor withMention={false} onChange={(tex, raw, html) => {}} />
+      <Select
+        className="multi-select"
+        classNamePrefix="react-select"
+        isMulti
+        menuPortalTarget={document.body}
+        value={assignedConnections}
+        onChange={(e) => {
+          console.log("EEE:", e);
+          setAssignedConnections(e);
+        }}
+        styles={{
+          control: (base) => ({ ...base, width: 400, marginBottom: 10 }),
+        }}
+        options={connections}
+        placeholder="Choose a Connection(s)"
+      />
       <Box d="flex" justify="space-between">
         <ButtonGroup>
           <Button appearance="primary">Add Search Result</Button>
           <Button appearance="subtle" iconBefore={<DocumentIcon />} />
-          <Button
-            appearance="subtle"
-            iconBefore={<InviteTeamIcon />}
-            onClick={() => setIsConnectionModalOpen(true)}
-          />
         </ButtonGroup>
         <Button onClick={() => setIsFormVisible(false)} appearance="subtle">
           Cancel
         </Button>
 
         {/* Murat's part starting here */}
-
-        {/* That's Shyngys's part */}
-        <ModalDialog
-          isOpen={isConnectionModalOpen}
-          setIsOpen={setIsConnectionModalOpen}
-          heading="Assign a User"
-          positiveLabel="Assign"
-          onClick={() => onAddConnection()}
-          width="small"
-          body={
-            <>
-              <TagGroup>
-                {connections?.map(
-                  (connection) =>
-                    connection &&
-                    connection.contact && (
-                      <tag
-                        appearance="rounded"
-                        text={`${connection.contact.first_name} ${connection.contact.first_name}`}
-                        onBeforeRemoveAction={() => {
-                          onDeleteConnection(connection.id);
-                          return false;
-                        }}
-                      />
-                    )
-                )}
-              </TagGroup>
-              <Select
-                className="multi-select"
-                classNamePrefix="react-select"
-                isMulti
-                menuPortalTarget={document.body}
-                value={assignedConnections}
-                onChange={(e) => {
-                  console.log("EEE:", e);
-                  //setAssignedConnections(e);
-                }}
-                styles={{
-                  menuPortal: (base) => ({ ...base, zIndex: 9999 }),
-                }}
-                //options={state.not_child_users}
-                placeholder="Choose a User(s)"
-              />
-            </>
-          }
-        />
       </Box>
     </form>
   );
