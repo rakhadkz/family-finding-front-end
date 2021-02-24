a8266e9bddecbf94ade89528b063e310390fa9a4
Shyngys Rakhad <dev.chinga@gmail.com>
2021-02-24 18:02:46 +0600

    divide: responsibilities

--- src/components/ChildProfile/tabs/FamilySearch/AddSearchResultForm.js
+++ src/components/ChildProfile/tabs/FamilySearch/AddSearchResultForm.js
@@ -1,12 +1,17 @@
 import Button, { ButtonGroup } from "@atlaskit/button";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { Box } from "../../../ui/atoms";
 import { SelectInput } from "../../../ui/molecules";
+import Select from "@atlaskit/select";
 import { WysiwygEditor } from "../../../WYSIWYG";
 import DocumentIcon from "@atlaskit/icon/glyph/document";
 import InviteTeamIcon from "@atlaskit/icon/glyph/invite-team";
 import { fetchSearchVectorsRequest } from "../../../../api/searchVectors/searchVectorsRequests";
+import { ModalDialog } from "../../../ui/common";
+//import tag from "@atlaskit/tag/dist/types/tag";
+import TagGroup from "@atlaskit/tag-group";
+import { ChildContext } from "../../../../pages/ChildProfilePage";
 
 export const AddSearchResultForm = ({
   currentSearchResult,
@@ -14,8 +19,14 @@ export const AddSearchResultForm = ({
 }) => {
   const { control } = useForm();
   const [options, setOptions] = useState([]);
+  const [isConnectionModalOpen, setIsConnectionModalOpen] = useState(false);
+  const [connections, setConnections] = useState(
+    useContext(ChildContext).connectionState.connections || []
+  );
+  const [assignedConnections, setAssignedConnections] = useState([]);
 
   useEffect(() => {
+    console.log("Connections:::", connections);
     fetchSearchVectors();
   }, []);
 
@@ -23,6 +34,16 @@ export const AddSearchResultForm = ({
     fetchSearchVectorsRequest({}).then((data) =>
       setOptions(data.map((item) => ({ label: item.name, value: item.id })))
     );
+  };
+
+  const onAddConnection = () => {
+    assignedConnections.map((item) =>
+      setConnections((prev) => [...prev, item])
+    );
+  };
+
+  const onDeleteConnection = (id) => {
+    setConnections((prev) => prev.filter((connection) => connection.id !== id));
   };
 
   return (
@@ -41,11 +62,63 @@ export const AddSearchResultForm = ({
         <ButtonGroup>
           <Button appearance="primary">Add Search Result</Button>
           <Button appearance="subtle" iconBefore={<DocumentIcon />} />
-          <Button appearance="subtle" iconBefore={<InviteTeamIcon />} />
+          <Button
+            appearance="subtle"
+            iconBefore={<InviteTeamIcon />}
+            onClick={() => setIsConnectionModalOpen(true)}
+          />
         </ButtonGroup>
         <Button onClick={() => setIsFormVisible(false)} appearance="subtle">
           Cancel
         </Button>
+
+        {/* Murat's part starting here */}
+
+        {/* That's Shyngys's part */}
+        <ModalDialog
+          isOpen={isConnectionModalOpen}
+          setIsOpen={setIsConnectionModalOpen}
+          heading="Assign a User"
+          positiveLabel="Assign"
+          onClick={() => onAddConnection()}
+          width="small"
+          body={
+            <>
+              <TagGroup>
+                {connections?.map(
+                  (connection) =>
+                    connection &&
+                    connection.contact && (
+                      <tag
+                        appearance="rounded"
+                        text={`${connection.contact.first_name} ${connection.contact.first_name}`}
+                        onBeforeRemoveAction={() => {
+                          onDeleteConnection(connection.id);
+                          return false;
+                        }}
+                      />
+                    )
+                )}
+              </TagGroup>
+              <Select
+                className="multi-select"
+                classNamePrefix="react-select"
+                isMulti
+                menuPortalTarget={document.body}
+                value={assignedConnections}
+                onChange={(e) => {
+                  console.log("EEE:", e);
+                  //setAssignedConnections(e);
+                }}
+                styles={{
+                  menuPortal: (base) => ({ ...base, zIndex: 9999 }),
+                }}
+                //options={state.not_child_users}
+                placeholder="Choose a User(s)"
+              />
+            </>
+          }
+        />
       </Box>
     </form>
   );
