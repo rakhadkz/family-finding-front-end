d9a36ca5786d7312ff8aad4750297621eaaaa2eb
Bekzat <bekzat@MacBook-Pro-Bekzat.local>
2021-03-02 20:57:12 +0600

    phone settings conected

--- src/api/settings/settings.js
+++ src/api/settings/settings.js
@@ -9,4 +9,18 @@ export const fetchPhoneNumbers = () => {
   };
   
   
+  export const fetchPhoneSettings = () => {
+    return request({
+      endpoint: `twilio_phone_numbers`,
+      method: "GET",
+    });
+  };
   
+  export const purchasePhone = (data) => {
+    return request({
+      endpoint: `choose_phone_number/0987654321poiuytrewq`,
+      method: "POST",
+      data,
+      isV1: false
+    });
+  };
--- src/components/Settings/PhoneSettings.js
+++ src/components/Settings/PhoneSettings.js
@@ -2,13 +2,29 @@ import Button from "@atlaskit/button";
 import Select from "@atlaskit/select";
 import Textfield from "@atlaskit/textfield";
 import React, { useEffect, useState } from "react";
-import { fetchPhoneNumbers } from "../../api/settings";
+import { toast } from "react-toastify";
+import {
+  fetchPhoneNumbers,
+  fetchPhoneSettings,
+  purchasePhone
+} from "../../api/settings";
+import { useAuth } from "../../context/auth/authContext";
 import { Box, Label, Spacing } from "../ui/atoms";
 import { ModalDialog } from "../ui/common";
 
 function PhoneSettings() {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [fetching, setFetching] = useState(false);
   const [numbers, setNumbers] = useState([]);
+  const [selectedPhone, setSelectedPhone] = useState(null);
+  const { user } = useAuth();
+  const [phone, setPhone] = useState({});
+
+  useEffect(() => {
+    fetchPhone();
+  }, []);
+
+  const fetchPhone = () => fetchPhoneSettings().then(setPhone);
 
   useEffect(() => {
     if (isModalOpen) {
@@ -20,6 +36,29 @@ function PhoneSettings() {
     fetchPhoneNumbers().then(setNumbers);
   };
 
+  const handlePhoneChange = () => {
+    console.log(selectedPhone, user);
+    setFetching(true)
+    purchasePhone({
+      phone: selectedPhone.value,
+      friendly_name: selectedPhone.label,
+      organization_id: user.selectedOrganization.value.id,
+    })
+      .then(() => {
+        toast.success("Changed successfully!");
+      })
+      .catch(() => {
+        toast.success("Catched error!");
+      })
+      .finally(() => {
+        setIsModalOpen(false);
+        fetchPhone();
+        setFetching(false)
+      });
+  };
+
+  console.log("PHONNEE", phone);
+
   return (
     <div>
       <Spacing m="30px 10px">
@@ -30,29 +69,32 @@ function PhoneSettings() {
               style={{ width: 350 }}
               disabled={true}
               name={"phone"}
-              value={`+13238706031`}
+              value={phone?.phone || `+13238706031`}
             />
           </Box>
 
           <Spacing m="23px 10px">
-            <Button
-              size="large"
-              style={{
-                height: 40,
-                alignItems: "center",
-                justifyContent: "center",
-              }}
-              onClick={() => setIsModalOpen(true)}
-              appearance="primary"
-            >
-              Change
-            </Button>
+            {phone?.phone ? null : (
+              <Button
+                size="large"
+                style={{
+                  height: 40,
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+                onClick={() => setIsModalOpen(true)}
+                appearance="primary"
+              >
+                Change
+              </Button>
+            )}
           </Spacing>
         </Box>
         <ModalDialog
           isOpen={isModalOpen}
           setIsOpen={setIsModalOpen}
-          onClick={() => setIsModalOpen(false)}
+          isLoading={fetching}
+          onClick={() => handlePhoneChange()}
           heading="Change the phone number"
           positiveLabel="Select"
           width="small"
@@ -61,7 +103,7 @@ function PhoneSettings() {
               <Select
                 className="multi-select"
                 classNamePrefix="react-select"
-                isMulti
+                onChange={setSelectedPhone}
                 menuPortalTarget={document.body}
                 styles={{
                   menuPortal: (base) => ({ ...base, zIndex: 9999 }),
