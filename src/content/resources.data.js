407c09399b29381c3c7efc83443d910744c04863
Bekzat <bekzat@MacBook-Pro-Bekzat.local>
2021-05-12 19:34:35 +0600

    resources front

---
+++ src/content/resources.data.js
@@ -0,0 +1,69 @@
+import Button, { ButtonGroup } from "@atlaskit/button";
+import CrossIcon from "@atlaskit/icon/glyph/cross";
+import EditIcon from "@atlaskit/icon/glyph/edit";
+import moment from "moment";
+import { ACTIONS } from "../accessControl/actions";
+import Can from "../accessControl/Can";
+import { COMMUNICATION_TEMPLATES } from "../helpers";
+import { humanReadableDateFormat } from "./date";
+
+const resourcesData = (
+  data,
+  setIsOpen,
+  setCurrentId,
+  setIsOpenEdit,
+  setCurrentResource
+) =>
+  data.map((item, index) => ({
+    key: index,
+    cells: [
+      {
+        key: "name",
+        content: item?.name,
+      },
+      {
+        key: "link",
+        content: <a onClick={() => window.open(item?.link)}>{item?.link}</a>,
+      },
+      {
+        key: "updated_at",
+        content: moment(item?.updated_at).format(humanReadableDateFormat),
+      },
+      {
+        key: "actions",
+        content: (
+          <div align="center">
+            <Can
+              perform={`${COMMUNICATION_TEMPLATES}:${ACTIONS.EDIT}`}
+              yes={() => (
+                <ButtonGroup>
+                  <Button
+                    onClick={() => {
+                      setIsOpenEdit(true);
+                      setCurrentResource(item);
+                    }}
+                    height="32px"
+                    width="32px"
+                  >
+                    <EditIcon size="small" />
+                  </Button>
+                  <Button
+                    onClick={() => {
+                      setIsOpen(true);
+                      setCurrentId(item.id);
+                    }}
+                    height="32px"
+                    width="32px"
+                  >
+                    <CrossIcon size="small" />
+                  </Button>
+                </ButtonGroup>
+              )}
+            />
+          </div>
+        ),
+      },
+    ],
+  }));
+
+export { resourcesData };
