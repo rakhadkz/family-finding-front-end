3a2ce2d372c3c2b1726ca01c9c075731b7dd1cdf
Murat Tishkul <murat.tishkul@gmail.com>
2021-05-17 19:12:05 +0600

    styled reports page

--- src/pages/ReportsPage.js
+++ src/pages/ReportsPage.js
@@ -10,11 +10,13 @@ import styled from 'styled-components'
 import Chartist from 'chartist';
 import 'chartist-plugin-axistitle'
 import 'chartist-plugin-accessibility'
+import ChartistAccessibility from 'react-chartist-plugin-accessibility'
+import 'chartist-plugin-tooltips-updated';
 
 export const ReportsPage = () => {
-  const [children, setChildren] = useState();
-  const [placements, setPlacements] = useState();
-  const [linkedConnections, setLinkedConnections] = useState();
+  const [children, setChildren] = useState({labels:[],series:[[]]});
+  const [placements, setPlacements] = useState({labels:[],series:[[]]});
+  const [linkedConnections, setLinkedConnections] = useState({labels:[],series:[[]]});
 
   /*
     labels: children.keys(),
@@ -32,7 +34,7 @@ export const ReportsPage = () => {
     console.log(data?.placements)
     setPlacements({
       labels: data?.placements.map(i => i[0]),
-      series: [  data?.placements.map(i => i[1]) ]
+      series: [ data?.placements.map(i => i[1])]
     })
   },[])
   const fetchLinkedConnections =  useCallback( async () => {
@@ -62,7 +64,7 @@ export const ReportsPage = () => {
     plugins: [
       Chartist.plugins.ctAxisTitle({
         axisX: {
-          axisTitle: "Time (mins)",
+          axisTitle: "Children",
           axisClass: "ct-axis-title",
           offset: {
             x: 0,
@@ -71,7 +73,7 @@ export const ReportsPage = () => {
           textAnchor: "middle"
         },
         axisY: {
-          axisTitle: "Total count",
+          axisTitle: "Number of Children",
           axisClass: "ct-axis-title",
           offset: {
             x: 0,
@@ -80,37 +82,168 @@ export const ReportsPage = () => {
           flipTitle: false
         }
       }),
-      Chartist.plugins.ctAccessibility({
-        caption: 'Fiscal year 2015',
-        seriesHeader: 'business numbers',
-        summary: 'A graphic that shows the business numbers of the fiscal year 2015',
-        valueTransform: function(value) {
-          return value + ' dollar';
+      Chartist.plugins.tooltip()
+      // Chartist.plugins.tooltip({
+      //   currency: '$',
+      //   class: 'class1 class2',
+      //   appendToBody: true
+      // })
+    ]
+  };
+
+  var optionsPlacement = {
+    // high: 10,
+    // low: -10,
+    // stackBars: true,
+    // axisX: {
+    //   labelInterpolationFnc: function(value, index) {
+    //     return index % 2 === 0 ? value : null;
+    //   }
+    // }
+    plugins: [
+      Chartist.plugins.ctAxisTitle({
+        axisX: {
+          axisTitle: "Contacts",
+          axisClass: "ct-axis-title",
+          offset: {
+            x: 0,
+            y: 50
+          },
+          textAnchor: "middle"
         },
-        visuallyHiddenStyles: 
-        'position: relative; top: 100%; width: 100%; font-size: 11px; overflow-x: auto; background-color: rgba(0, 0, 0, 0.1); padding: 10px'
+        axisY: {
+          axisTitle: "Number of Placed Contacts",
+          axisClass: "ct-axis-title",
+          offset: {
+            x: 0,
+            y: -1
+          },
+          flipTitle: false
+        }
+      }),
+      Chartist.plugins.tooltip({
+        currency: '$',
+        class: 'class1 class2',
+        appendToBody: true
       })
     ]
   };
 
-  var type = 'Bar'
+  var optionsLinkedConnections = {
+    // high: 10,
+    // low: -10,
+    // stackBars: true,
+    // axisX: {
+    //   labelInterpolationFnc: function(value, index) {
+    //     return index % 2 === 0 ? value : null;
+    //   }
+    // }
+    plugins: [
+      Chartist.plugins.ctAxisTitle({
+        axisX: {
+          axisTitle: "Contacts",
+          axisClass: "ct-axis-title",
+          offset: {
+            x: 0,
+            y: 50
+          },
+          textAnchor: "middle"
+        },
+        axisY: {
+          axisTitle: "Number of Linked Connections",
+          axisClass: "ct-axis-title",
+          offset: {
+            x: 0,
+            y: -1
+          },
+          flipTitle: false
+        }
+      }),
+      Chartist.plugins.tooltip({
+        currency: '$',
+        class: 'class1 class2',
+        appendToBody: true
+      })
+    ]
+  };
+
   var aspectRatio = 'ct-octave';
-
-
   return (
     <>
       <Title>Reports</Title>
       <Spacing m={{ t: "23px" }}>
+        
         <Box d="flex" justify="space-between">
-          
+        
 
         </Box>
       </Spacing>
       <Spacing m={{ t: "20px" }}>
-        
-        <StyledChart data={children} options={options} type={type} />
-        <StyledChart data={placements} options={options} type={type} />
-        <StyledChart data={linkedConnections} options={options} type={type} />
+        <div style={{marginBottom: '200px'}}>
+        <StyledChart className={aspectRatio} data={children} options={options} type={'Bar'} >
+          <ChartistAccessibility
+            caption={'Total number of Children in a system'}
+            summary={'A graphic that shows the total number of children in the system by months of the last year'}
+            seriesHeader={'New children registered'}
+            visuallyHiddenStyles={{
+              position: 'absolute',
+              top: '100%',
+              width: '100%',
+              backgroundColor: 'rgba(0, 0, 0, 0.1)',
+              fontSize: '14px',
+              paddingLeft: '10px',
+              paddingRight: '10px'
+            }}
+            valueTransform={function(value) {
+              return value + (value === 1 ? ' child' : ' children');
+            }}
+          />
+        </StyledChart>
+        </div>
+        <div style={{marginBottom: '200px'}}>
+
+        <StyledChart className={aspectRatio} data={placements} options={optionsPlacement} type={'Bar'} >
+        <ChartistAccessibility
+            caption={'Total number of Placed Contacts in a system'}
+            summary={'A graphic that shows the total number of placed contacts in the system by months of the last year'}
+            seriesHeader={'New children registered'}
+            visuallyHiddenStyles={{
+              position: 'absolute',
+              top: '100%',
+              width: '100%',
+              backgroundColor: 'rgba(0, 0, 0, 0.1)',
+              fontSize: '14px',
+              paddingLeft: '10px',
+              paddingRight: '10px'
+            }}
+            valueTransform={function(value) {
+              return value + (value === 1 ? ' contact' : ' contacts');
+            }}
+          />
+        </StyledChart>
+        </div>
+
+        <div style={{marginBottom: '200px'}}>
+          <StyledChart className={aspectRatio} data={linkedConnections} options={optionsLinkedConnections} type={'Bar'}>
+          <ChartistAccessibility
+            caption={'Total number of Linked Connections in a system'}
+            summary={'A graphic that shows the total number of linked contacts in the system by months of the last year'}
+            seriesHeader={'New contacts linked'}
+            visuallyHiddenStyles={{
+              position: 'absolute',
+              top: '100%',
+              width: '100%',
+              backgroundColor: 'rgba(0, 0, 0, 0.1)',
+              fontSize: '14px',
+              paddingLeft: '10px',
+              paddingRight: '10px'
+            }}
+            valueTransform={function(value) {
+              return value + (value === 1 ? ' contact' : ' contacts');
+            }}
+          />
+          </StyledChart>
+        </div>
 
       </Spacing>
     </>
